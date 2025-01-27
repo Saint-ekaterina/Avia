37983be6799625a573bb3442f0e3628918bffdfc
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-01-27 17:33:12 +0700

    бонусные мили

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int x = 15_600; //стоимость билета
+        int y = 20; // бонусные мили
+
+        System.out.println(x / y); // колличество бонусных миль от стоимости билета
+    }
 }
